3d2b06eb85a5423474515c5b4bd7887c22f20123
Batuhan Arslan <81113714+mirarsl@users.noreply.github.com>
2024-09-16 16:39:23 +0300

    logolar sayfası

---
+++ resources/views/modules/logos.blade.php
@@ -0,0 +1,88 @@
+<section class="basindabiz">
+ <div class="container">
+  <h4 style="margin-top: 0px;">OSGB Türkçe</h4>
+  <div class="row">
+   <div class="col-md-3 col-6">
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail" style="background-color: black;">
+     <img src="{{asset(setting('site.logo_turkce_yatay_beyaz'))}}" class="img-responsive" alt="Türkçe Yatay Beyaz Logo" title="Türkçe Yatay Beyaz Logo">
+    </div>
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail">
+     <img src="{{asset(setting('site.logo_turkce_yatay_siyah'))}}" class="img-responsive" alt="Türkçe Yatay Siyah Logo" title="Türkçe Yatay Siyah Logo">
+    </div>
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail">
+     <img src="{{asset(setting('site.logo_turkce_yatay_bordo'))}}" class="img-responsive" alt="Türkçe Yatay Bordo Logo" title="Türkçe Yatay Bordo Logo">
+    </div>
+   </div>
+  </div>
+
+  <div class="row">
+   <div class="col-md-3 col-6">
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail" style="background-color: black;">
+     <img src="{{asset(setting('site.logo_turkce_dikey_beyaz'))}}" class="img-responsive" alt="Türkçe Dikey Beyaz Logo" title="Türkçe Dikey Beyaz Logo">
+    </div>
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail">
+     <img src="{{asset(setting('site.logo_turkce_dikey_siyah'))}}" class="img-responsive" alt="Türkçe Dikey Siyah Logo" title="Türkçe Dikey Siyah Logo">
+    </div>
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail">
+     <img src="{{asset(setting('site.logo_turkce_dikey_bordo'))}}" class="img-responsive" alt="Türkçe Dikey Bordo Logo" title="Türkçe Dikey Bordo Logo">
+    </div>
+   </div>
+  </div>
+
+  <h4 style="margin-top: 10px;">OSGB İngilizce</h4>
+  <div class="row">
+   <div class="col-md-3 col-6">
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail" style="background-color: black;">
+     <img src="{{asset(setting('site.logo_ingilizce_yatay_beyaz'))}}" class="img-responsive" alt="İngilizce Yatay Beyaz Logo" title="İngilizce Yatay Beyaz Logo">
+    </div>
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail">
+     <img src="{{asset(setting('site.logo_ingilizce_yatay_siyah'))}}" class="img-responsive" alt="İngilizce Yatay Siyah Logo" title="İngilizce Yatay Siyah Logo">
+    </div>
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail">
+     <img src="{{asset(setting('site.logo_ingilizce_yatay_bordo'))}}" class="img-responsive" alt="İngilizce Yatay Bordo Logo" title="İngilizce Yatay Bordo Logo">
+    </div>
+   </div>
+  </div>
+
+  <div class="row">
+   <div class="col-md-3 col-6">
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail" style="background-color: black;">
+     <img src="{{asset(setting('site.logo_ingilizce_dikey_beyaz'))}}" class="img-responsive" alt="İngilizce Dikey Beyaz Logo" title="İngilizce Dikey Beyaz Logo">
+    </div>
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail">
+     <img src="{{asset(setting('site.logo_ingilizce_dikey_siyah'))}}" class="img-responsive" alt="İngilizce Dikey Siyah Logo" title="İngilizce Dikey Siyah Logo">
+    </div>
+   </div>
+   <div class="col-md-3 col-6">
+    <div class="thumbnail">
+     <img src="{{asset(setting('site.logo_ingilizce_dikey_bordo'))}}" class="img-responsive" alt="İngilizce Dikey Bordo Logo" title="İngilizce Dikey Bordo Logo">
+    </div>
+   </div>
+  </div>
+
+  
+
+ </div>
+</section>

ebdd939f0cce4e25129e40fd1af7573361535c94
Batuhan Arslan <81113714+mirarsl@users.noreply.github.com>
2024-09-23 20:25:28 +0300

    Update blog-details.blade.php

--- resources/views/details/blog-details.blade.php
+++ resources/views/details/blog-details.blade.php
@@ -3,11 +3,12 @@
 <section class="blog">
  <div class="container">
   <div class="row">
-   @if($Page->image != null)
    <div class="col-md-4 left-side">
+    @if($Page->image != null)
     <div class="thumbnail">
      <img src="{{asset($Page->image)}}" alt="{{$Page->title}}" class="img-responsive">
     </div>
+    @endif
     <div class="writer-area">
      @if($Page->author != null)
      <p><i class="fa fa-user"></i> <b>Yazan:</b> {{$Page->author}}</p>
@@ -15,7 +16,6 @@
      <p><i class="fa fa-calendar"></i> <b>Tarih:</b> {{$Page->date('d/m/Y')}}</p>
     </div>
    </div>
-   @endif
    <div class="{{$Page->image != null ? 'col-md-8' : 'col-md-12'}}">
     {!! $Page->text !!}
     

9f7817d010b70718e10f3b478a8f7789cd5ceb4f
Batuhan Arslan <81113714+mirarsl@users.noreply.github.com>
2024-09-29 16:50:15 +0300

    Update default.blade.php

--- resources/views/modules/default.blade.php
+++ resources/views/modules/default.blade.php
@@ -2,7 +2,7 @@
  <div class="container">
   <div class="row">
    <div class="col-md-12">
-    <h2>{{$module->title}}</h2>
+    <h2>{!! $module->title!!}</h2>
     {!! $module->text !!}
    </div> 
   </div>

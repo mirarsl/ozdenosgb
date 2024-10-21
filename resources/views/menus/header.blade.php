8c4a5ad65af429150984976b4ec12a9a15400653
Batuhan Arslan <81113714+mirarsl@users.noreply.github.com>
2024-10-21 08:25:57 +0300

    Update header.blade.php

--- resources/views/menus/header.blade.php
+++ resources/views/menus/header.blade.php
@@ -4,7 +4,7 @@ empty($stack) ?  $stack = 0 :''
 <ul @if ($stack == 0) class="nav navbar-nav navbar-right" @else class="dropdown-menu" @endif>
 	@foreach ($items as $menu_item)
 	<li class="{{ count($menu_item->children) > 0 || isset($menu_item->app_model) ? 'dropdown' : '' }}">
-		<a {{ count($menu_item->children) > 0 || isset($menu_item->app_model) ? 'class="dropdown-toggle"' : '' }} href='{{ !isset($menu_item->app_model) ? $menu_item->link() : (isset($menu_item->parameters) ? route('page',$menu_item->parameters->slug) : 'javascript:void(0);') }}'>{{ $menu_item->title }} {!! count($menu_item->children) > 0 || isset($menu_item->app_model) ? '<b class="caret"></b>' : '' !!} {!! $menu_item->icon_class != null ? '<i class="'.$menu_item->icon_class.'"></i>':'' !!} </a>
+		<a {{ count($menu_item->children) > 0 || isset($menu_item->app_model) ? 'class="dropdown-toggle"' : '' }} href='{{ !isset($menu_item->app_model) ? $menu_item->link() : (isset($menu_item->parameters) ? route('page',$menu_item->parameters->slug) : 'javascript:void(0);') }}' target="{{$menu_item->target}}">{{ $menu_item->title }} {!! count($menu_item->children) > 0 || isset($menu_item->app_model) ? '<b class="caret"></b>' : '' !!} {!! $menu_item->icon_class != null ? '<i class="'.$menu_item->icon_class.'"></i>':'' !!} </a>
 		@if (count($menu_item->children) > 0 || isset($menu_item->app_model))
 		@if (count($menu_item->children) > 0)
 		@include('menus.header', ['items' => $menu_item->children, 'stack' => $stack + 1])
@@ -22,7 +22,7 @@ empty($stack) ?  $stack = 0 :''
 			}
 			@endphp
 			@foreach ($model->get() as $item)
-			<li><a href="{{route($menu_item->route,$item->slug)}}">{{$item->title}}</a></li>
+			<li><a href="{{route($menu_item->route,$item->slug)}}" target="{{$menu_item->target}}">{{$item->title}}</a></li>
 			@endforeach
 		</ul>
 		@endif
